cf7d2cfbde6351eaf9128b80060b4bcba897b6bb
Doreen <doreenlulu97@gmail.com>
2022-09-08 01:30:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+	let users = {
+		name: name,
+		email: email,
+	};
+	return fetch("http://localhost:3000/users", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: JSON.stringify(users),
+	})
+        // Response and fetching
+
+		.then((response) => response.json())
+		.then((fetchedobject) => {
+			document.body.innerHTML = fetchedobject["id"];
+		})
+
+        // Error
+		.catch((error) => {
+			document.body.innerHTML = error.message;
+		});
+}
